a38d32a67275dde645dc05064b47bf887438cc15
1354478648 <1354478648@qq.com>
2024-02-01 15:47:14 +0800

    2024-02-01:初步实现高德API逆地理编码

--- utility/gaode.go
+++ utility/gaode.go
@@ -11,7 +11,7 @@ import (
 )
 
 // 封装返回结果
-type Response struct {
+type GeocodeResponse struct {
 	Geocodes []Geocode `json:"geocodes"`
 }
 
@@ -24,6 +24,27 @@ type Geocode struct {
 	Street           string      `json:"street" dc:"街道"`
 	Number           interface{} `json:"number" dc:"门牌号"`
 	Location         string      `json:"location" dc:"经纬度"`
+}
+
+type ReGeocodeResponse struct {
+	ReGeocode ReGeocode `json:"regeocode"`
+}
+
+type ReGeocode struct {
+	AddressComponent AddressComponent `json:"addressComponent"`
+	FormattedAddress string           `json:"formatted_address"`
+}
+
+type AddressComponent struct {
+	Province     string       `json:"province"`
+	City         string       `json:"city"`
+	District     string       `json:"district"`
+	StreetNumber StreetNumber `json:"streetNumber"`
+}
+
+type StreetNumber struct {
+	Number string `json:"number"`
+	Street string `json:"street"`
 }
 
 // 地理编码
@@ -47,7 +68,7 @@ func Geocoding(address, city string) (geocode *Geocode, err error) {
 	}
 
 	// 解析JSON数据
-	var response Response
+	var response GeocodeResponse
 	err = json.Unmarshal([]byte(body), &response)
 	if err != nil {
 		return nil, gerror.New("解析JSON数据时发生错误")
@@ -62,4 +83,34 @@ func Geocoding(address, city string) (geocode *Geocode, err error) {
 
 // 逆向地理编码
 // 经纬度 -> 结构化地址
-// func ReGeocoding()
+func ReGeocoding(lnglat string) (reGeocode *ReGeocode, err error) {
+	// 设置高德地图地理编码API的URL和参数
+	url := "https://restapi.amap.com/v3/geocode/regeo"
+	parameters := fmt.Sprintf("?key=%s&location=%s", consts.GaodeKey, lnglat)
+	fmt.Println("-------------")
+	fmt.Println(url + parameters)
+	// 发送HTTP请求
+	result, err := http.Get(url + parameters)
+	if err != nil {
+		return nil, gerror.New("发送高德API请求时发生错误")
+	}
+	defer result.Body.Close()
+	fmt.Println("-------------")
+	fmt.Println(result.Body)
+	// 读取响应内容
+	body, err := io.ReadAll(result.Body)
+	if err != nil {
+		return nil, gerror.New("读取高德API响应时发生错误")
+	}
+	fmt.Println("-------------")
+	fmt.Println(string(body))
+	// 解析JSON数据
+	var response ReGeocodeResponse
+	err = json.Unmarshal([]byte(body), &response)
+	if err != nil {
+		return nil, gerror.New("解析JSON数据时发生错误")
+	}
+
+	fmt.Println(&response.ReGeocode)
+	return &response.ReGeocode, nil
+}
